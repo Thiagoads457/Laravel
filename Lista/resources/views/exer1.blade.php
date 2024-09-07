479c058b992e6c2a86253d25195f4c5f06d60678
Thiagoads457 <thiagoads457@gmail.com>
2024-09-07 19:05:39 -0300

    1905

--- Lista/resources/views/exer1.blade.php
+++ Lista/resources/views/exer1.blade.php
@@ -7,11 +7,28 @@
     <title>Exercício 1</title>
 </head>
 <body>
-    <form action="/respostaExer1" method="POST">
+<div class="container mt-5">
+
+<h2 class="text-center mb-4">Calcular Adição</h2>
+    <form action="/respostaExer1" method="POST" class="border p-4 rounded">
         @CSRF
-        <input type="number" name="valor1" >
-        <input type="number" name="valor2" >
-        <button type="submit">Calcular</button>
+        <div class="row mb-3">
+            <div class="col-md-4">
+                <label for="valor1" class="form-label">1º Número</label>
+                <input type="number" name="valor1" id="valor1" class="form-control" placeholder="Inserir Número" required>
+            </div>
+            <div class="col-md-4">
+                <label for="valor2" class="form-label">2º Número</label>
+                <input type="number" name="valor2" id="valor2" class="form-control" placeholder="Inserir Número" required>
+            
+        </div>
+
+        <div class="row">
+            <div class="col-md-12">
+                <button type="submit" class="btn btn-success mt-2">Calcular</button>
+            </div>
+        </div>
     </form>
+</div>
 </body>
 </html>
--- Lista/resources/views/exer2.blade.php
+++ Lista/resources/views/exer2.blade.php
@@ -7,12 +7,29 @@
     <title>Exercício 2</title>
 </head>
 <body>
-    <form action="/respostaExer2" method="POST">
+<div class="container mt-5">
+
+<h2 class="text-center mb-4">Calcular Subtração</h2>
+    <form action="/respostaExer2" method="POST" class="border p-4 rounded">
         @CSRF
-        <input type="numer" name="valor1">
-        <input type="numer" name="valor2">
-        <button type="submit">Calcular</button>
+        <div class="row mb-3">
+            <div class="col-md-4">
+                <label for="valor1" class="form-label">1º Número</label>
+                <input type="number" name="valor1" id="valor1" class="form-control" placeholder="Inserir Número" required>
+            </div>
+            <div class="col-md-4">
+                <label for="valor2" class="form-label">2º Número</label>
+                <input type="number" name="valor2" id="valor2" class="form-control" placeholder="Inserir Número" required>
+            
+        </div>
+
+        <div class="row">
+            <div class="col-md-12">
+                <button type="submit" class="btn btn-success mt-2">Calcular</button>
+            </div>
+        </div>
     </form>
+</div>
     
 </body>
 </html>
